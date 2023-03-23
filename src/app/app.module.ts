2d5d0b2a5c1f3ec44c32fb234692ac2e18ee8391
William Dias Marcos <william.diasmarcos@gmail.com>
2023-03-22 21:49:36 -0300

    initial setting

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './component/header/header.component';
+import { DolarComponent } from './component/dolar/dolar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    DolarComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/component/dolar/dolar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dolar',
+  templateUrl: './dolar.component.html',
+  styleUrls: ['./dolar.component.css']
+})
+export class DolarComponent {
+
+}
